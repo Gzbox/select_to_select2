403fd6a67092be6e55d611113e6c51208542aea7
ding.zhen <ding.zhen@genecast.com.cn>
2022-05-01 19:09:16 +0800

    chore: upgrade the select2 from `3.4.5` to `3.4.6`. fix: fix incompatible `redmine 4.2.3`.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Select To Select2 Plugin
+
+This is a repository of 「Select To Select2 Plugin」 which is a Redmine plugin.
+
+## Version description
+
+Upgrade the select2 from `3.4.5` to `3.4.6`.
+Fix incompatible `redmine 4.2.3`.
+Self-test `redmine 4.2.3` operates normally. Other versions are not tested
+
+## Features
+
+Replace all select tag to select2 in all Redmine's Pages.
+
+![2017-10-09 21 50 40](https://user-images.githubusercontent.com/12267699/31339056-998c52f0-ad3c-11e7-88ea-bc7acf8cdf96.png)
+
+## Installation
+
+1.`$git clone https://github.com/sf-cola/select_to_select2.git`
+
+2.Copy to `apps/redmine/htdocs/plugins`
+
+3.`bundle exec rake redmine:plugins:migrate NAME=select_to_select2 RAILS_ENV=production`
---
+++ assets/javascripts/slecttoselect2main.js
@@ -0,0 +1,67 @@
+/*
+ Select To Select2 Plugin Main.
+ */
+
+(function ($) {
+    // For Page Load
+    replaceAllSelect2();
+
+    // For Ajax
+    $(document).ajaxComplete(function (event) {
+        replaceAllSelect2();
+    });
+
+    // For GET,POST Request
+    $(window).load(function () {
+        replaceAllSelect2();
+    });
+
+    // for toggle multi select elements
+    $(document).on("click", function (event) {
+        // console.log('document on click:%o', event.target.classList);
+        if (
+            event.target.classList &&
+            event.target.className.includes("toggle-multiselect")
+        ) {
+            replaceAllSelect2();
+        }
+    });
+
+    // for all elements
+    $(document).change(function (event) {
+        replaceAllSelect2();
+    });
+})(jQuery);
+
+function replaceAllSelect2() {
+    var elements = document.getElementsByTagName("select");
+
+    for (i = 0; i < elements.length; i++) {
+        // For not woroking 「width:resolve」
+        if (
+            elements[i].id == "year" ||
+            elements[i].id == "month" ||
+            elements[i].id == "columns" ||
+            elements[i].id == "settings_issuequery_query_id" ||
+            elements[i].id == "block-select"
+        ) {
+            $("#" + elements[i].id).select2({
+                width: "175px",
+                placeholder: "",
+                allowClear: true,
+            });
+        } else if (
+            elements[i].id == "available_c" ||
+            elements[i].id == "selected_c"
+        ) {
+            //do nothing, fix incorrect display
+        } else {
+            // For All Pages
+            $("#" + elements[i].id).select2({
+                width: "resolve",
+                placeholder: "",
+                allowClear: true,
+            });
+        }
+    }
+}
